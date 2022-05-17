b4f76ebbec5e84fc62afaa70ebd8e5b8d86df321
SOTODiLah <44207251+SOTODiLah@users.noreply.github.com>
2022-05-17 03:24:19 +0300

    Modification.

--- Test_Signature/FileBlock.hpp
+++ Test_Signature/FileBlock.hpp
@@ -28,14 +28,18 @@ public:
 	FileBlock& operator=(const FileBlock& fb)
 	{
 		str = fb.str;
+		hash = fb.hash;
 		posInFile = fb.posInFile;
+		posInHashFile = fb.posInHashFile;
 		return *this;
 	}
 
 	FileBlock& operator=(FileBlock&& fb) noexcept
 	{
 		str = std::move(fb.str);
-		posInFile = fb.posInFile;
+		hash = std::move(fb.hash);
+		posInFile = std::move(fb.posInFile);
+		posInHashFile = std::move(fb.posInHashFile);
 		return *this;
 	}
 
@@ -63,7 +67,41 @@ public:
 		hash.Final((CryptoPP::byte*)&digest[0]);
 	}
 
+	using HashCallback = std::function<std::shared_ptr<std::string>(const std::string&)>;
+	void hashing(HashCallback& callback, const size_t& sizeBlock)
+	{
+		if (!str)
+			throw std::exception("String not init.");
+
+		auto&& t = callback(*str);
+		if (!t)
+			throw std::exception("Hash not init.");
+
+		hash = std::move(t);
+
+		uint64_t&& strSize = str->size();
+
+		if (strSize == 0)
+			throw std::exception("String size is 0.");
+
+		posInHashFile = (posInFile / sizeBlock) * hash->size();
+	}
+
+	const std::string& getHash() const
+	{
+		if (!hash)
+			throw std::exception("Hash not init.");
+		return *hash;
+	}
+
+	const uint64_t& getPosHash() const
+	{
+		return posInHashFile;
+	}
+
 private:
 	std::shared_ptr<std::string> str;
+	std::shared_ptr<std::string> hash;
 	uint64_t posInFile = 0;
+	uint64_t posInHashFile = 0;
 };
